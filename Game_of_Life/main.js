81a34269b0855858f90dc9bdb3987ecb57ed099b
Ojka91 <ojkalp@gmail.com>
2019-03-22 22:33:07 +0100

    algorithm fixed. Some improvements done

--- Game_of_Life/main.js
+++ Game_of_Life/main.js
@@ -4,65 +4,112 @@ var myApp = new Vue({
         rows: [],
         columns: [],
         board: [],
+        lifeChanging: [],
+        gameStart: false,
+        gameRunning: "",
+       
     },
 
     methods: {
-        tableSize() {
-            for (x = 0; x < 100; x++) {
+        generateBoard() {
+            this.gameStart = true;
+            col = document.getElementById("columns").value;
+            row = document.getElementById("rows").value;
+            this.tableSize(row, col);
+        },
+        tableSize(row, col) {
+            for (x = 0; x < row; x++) {
                 this.rows.push(x)
-                this.columns.push(x)
+            }
+            for (y = 0; y < col; y++) {
+                this.columns.push(y)
             }
             this.createBoard();
         },
-
+        createRandomGame(){
+            for (var x = 0; x < this.rows.length; x++) {
+                for (var j = 0; j < this.columns.length; j++) {
+                    var random = Math.random() >= 0.9;
+                    this.board[x][j] = random;
+                }
+            }
+            this.printTable();
+        },
         createBoard() {
             for (var x = 0; x < this.rows.length; x++) {
                 this.board[x] = [];
                 for (var j = 0; j < this.columns.length; j++) {
-                    var random = Math.random() >= 0.95;
-                    this.board[x][j] = random;
+                    this.board[x][j] = false;
                 }
             }
-            setTimeout(this.printTable, 1500);
-
+        },
+        clearBoard(){
+            for (var x = 0; x < this.rows.length; x++) {
+                for (var j = 0; j < this.columns.length; j++) {
+                    this.board[x][j] = false;
+                    if(document.getElementById(x + "S" + j).classList.contains("cellLive")){
+                        document.getElementById(x + "S" + j).classList.remove("cellLive")
+                    }
+                }
+            }
+            clearInterval(this.gameRunning);
         },
         printTable() {
-            document.getElementById("10S10").classList.add("cellLive");
             for (var x = 0; x < this.rows.length; x++) {
                 for (var j = 0; j < this.columns.length; j++) {
                     var print = this.board[x][j];
                     if (print) {
                         document.getElementById(x + "S" + j).classList.add("cellLive");
                     }
+                    else{
+                        if(document.getElementById(x + "S" + j).classList.contains("cellLive")){
+                            document.getElementById(x + "S" + j).classList.remove("cellLive")
+                        }
+                    }
                 }
             }
-            setInterval(this.startGame, 100);
-
+        
+        },
+        drawPattern(rows, columns){
+            if(this.board[rows][columns]){
+                document.getElementById(rows+"S"+columns).classList.remove("cellLive")
+                this.board[rows][columns] = !this.board[rows][columns]
+            }else{
+                document.getElementById(rows+"S"+columns).classList.add("cellLive")
+                this.board[rows][columns] = !this.board[rows][columns]
+            }
+        },
+        start(){
+            this.gameRunning = setInterval(this.startGame, 500);
+        },
+        stop(){
+            clearInterval(this.gameRunning);
         },
         startGame() {
-            var liveChange = [];
+            
+            this.lifeChanging = [];
             for (var x = 0; x < this.rows.length; x++) {
                 for (var j = 0; j < this.columns.length; j++) {
                     var cp = this.board[x][j];
-                    if (x < 99) {
-                        var neighbor1 = this.board[x + 1][j];
+                    if (x < this.rows.length-1) {
+                        var neighbor1 = this.board[x +1][j];
                     }
                     if (x != 0) {
                         var neighbor2 = this.board[x - 1][j];
                     }
-                    if (j < 99) {
+                    if (j < this.columns.length) {
                         var neighbor3 = this.board[x][j + 1];
                     }
                     if (j != 0) {
                         var neighbor4 = this.board[x][j - 1];
                     }
-                    if (x < 99 && j < 99) {
-                        var neighbor5 = this.board[x + 1][j + 1];
+                    if (x < this.rows.length-1 && j < this.columns.length) {
+                        var neighbor5 = this.board[x +1][j + 1];
                     }
-                    if (x != 0 && j < 99) {
+                    if (x != 0 && j < this.columns.length) {
                         var neighbor6 = this.board[x - 1][j + 1];
                     }
-                    if (x < 99 && j != 0) {
+                    if (x < this.rows.length-1 && j != 0) {
                         var neighbor7 = this.board[x + 1][j - 1];
                     }
                     if (x != 0 && j != 0) {
@@ -71,25 +118,31 @@ var myApp = new Vue({
                     var neighbors = [neighbor1, neighbor2, neighbor3, neighbor4, neighbor5, neighbor6, neighbor7, neighbor8];
                     if (cp) {
                         if (neighbors.filter(Boolean).length >= 4 || neighbors.filter(Boolean).length <= 1) {
-                            this.board[x][j] = false;
-                            document.getElementById(x + "S" + j).classList.remove("cellLive");
+                            this.lifeChanging.push([x, j]);
+                           // document.getElementById(x + "S" + j).classList.remove("cellLive");
                         }
                     }
                     if (cp == false) {
                         if (neighbors.filter(Boolean).length == 3) {
-                            this.board[x][j] = true;
-
-                            document.getElementById(x + "S" + j).classList.add("cellLive");
+                            this.lifeChanging.push([x, j]);
+                            //document.getElementById(x + "S" + j).classList.add("cellLive");
                         }
                     }
                 }
             }
+            
+            for (var q = 0; q < this.lifeChanging.length; q++) {
+
+                this.board[this.lifeChanging[q][0]][this.lifeChanging[q][1]] = !this.board[this.lifeChanging[q][0]][this.lifeChanging[q][1]]
+               
+            }
+            this.printTable();
         }
 
     },
 
     created: function () {
-        this.tableSize();
+
     },
 
     mounted: function () {
