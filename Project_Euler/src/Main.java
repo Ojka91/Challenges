bb027138babd711c098bedc0970275c36d56eede
Ojka91 <oscar.lucena.papiol@es.ey.com>
2019-04-11 13:18:22 +0200

    Problem 002 updated

--- Project_Euler/src/Main.java
+++ Project_Euler/src/Main.java
@@ -2,7 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
     Project_Euler_001 p1 = new Project_Euler_001(1000);
+    Project_Euler_002 p2 = new Project_Euler_002(1,2);
+
         System.out.println("Solutions:");
         System.out.println("Project Euler 1: " + p1.findNaturalNumbers());
+        p2.start();
+        System.out.println("Project Euler 2: " + p2.getResult());
     }
 }
